1820b3994585788aec9fde0b69366daaf1b83a3a
Seventesprit <seventesprit@gmail.com>
2025-03-28 17:20:52 +0100

    lekcja_2

--- projekt.py
+++ projekt.py
@@ -2,6 +2,8 @@ import pygame
 import random
 import time
 from Jablko import Jablko
+from Kierunek import Kierunek
+from Waz import Waz
 
 SZEROKOSC_EKRANU = 800
 WYSOKOSC_EKRANU = 608
@@ -20,6 +22,10 @@ pygame.init()
 ekran = pygame.display.set_mode([SZEROKOSC_EKRANU, WYSOKOSC_EKRANU])
 zegar = pygame.time.Clock()
 
+waz = Waz()
+PORUSZ_WEZEM = pygame.USEREVENT + 1
+pygame.time.set_timer(PORUSZ_WEZEM, 200)
+
 jablko = Jablko()
 jablka = pygame.sprite.Group()
 jablka.add(jablko)
@@ -31,10 +37,23 @@ while gra_dziala:
             if zdarzenie.key == pygame.K_ESCAPE:
                 gra_dziala = False
 
+            if zdarzenie.key == pygame.K_w:
+                waz.zmien_kierunek(Kierunek.GORA)
+            if zdarzenie.key == pygame.K_a:
+                waz.zmien_kierunek(Kierunek.LEWO)
+            if zdarzenie.key == pygame.K_s:
+                waz.zmien_kierunek(Kierunek.DOL)
+            if zdarzenie.key == pygame.K_d:
+                waz.zmien_kierunek(Kierunek.PRAWO)
+
+        elif zdarzenie.type == PORUSZ_WEZEM:
+            waz.aktualizuj()
+
         elif zdarzenie.type == pygame.QUIT:
             gra_dziala = False
 
     ekran.blit(tlo, (0, 0))
+    ekran.blit(waz.obraz, waz.rect)
     for jablko in jablka:
         ekran.blit(jablko.obraz, jablko.rect)
     
